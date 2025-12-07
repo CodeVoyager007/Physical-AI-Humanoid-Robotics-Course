6d795e7b9c865bda710db3b95e207de93e215587
Ayesha Mughal <162779681+CodeVoyager007@users.noreply.github.com>
2025-12-08 00:09:47 +0500

    Set baseUrl to '/' and deploymentBranch to 'gh-pages'

--- book/docusaurus.config.ts
+++ book/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   url: 'https://CodeVoyager007.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Physical-AI-Humanoid-Robotics-Course/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'CodeVoyager007',
